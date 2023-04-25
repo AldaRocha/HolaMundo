94a6cbb53de4ec437e85d131b8ea3653275e5d31
Francisco Javier Rocha Aldana <rochaaldanafcojavier@gmail.com>
2023-04-24 19:57:22 -0600

    Primer commit

---
+++ src/org/utl/dsm/HolaMundo.java
@@ -0,0 +1,12 @@
+
+package org.utl.dsm;
+
+/**
+ *
+ * @author rocha
+ */
+public class HolaMundo{
+    public static void main(String[] args){
+        System.out.println("¡Hola Mundo!");
+    }   
+}
